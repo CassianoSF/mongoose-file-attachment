3ab218a9df854f879b7dfa4710c53487aabcb6e7
Tiago Debastiani <tiago.debastiani@compasso.com.br>
2021-06-30 15:14:27 -0300

    Corrige Atualizar ao salvar

--- src/Controller.ts
+++ src/Controller.ts
@@ -24,48 +24,48 @@ export default class Controller {
     this.schema = schema
   }
 
-    private updateFileAttachments(
-        originalDoc: Document,
-        modifiedDoc: UpdateQuery<FileAttachment> | UpdateWithAggregationPipeline | null,
-        schemaObj: any,
-        rmCb: AttachCallback,
-        mkCb: AttachCallback,
-    ): Promise<void>[] {
-        const promises: Promise<void>[] = []
-        if (modifiedDoc && typeof modifiedDoc === 'object') {
-            if (Array.isArray(modifiedDoc)) {
-                Object.keys(modifiedDoc).map((key) =>
-                    promises.push(
-                        ...this.updateFileAttachments(
-                            originalDoc[key], modifiedDoc[key], schemaObj[key], rmCb, mkCb,
-                        ),
-                    ))
-            }
-            Object.keys(modifiedDoc).forEach((key) => {
-                if (schemaObj == undefined) return
-                if (modifiedDoc[key] instanceof FileAttachment) {
-                    promises.push(mkCb({
-                        data: modifiedDoc[key],
-                        options: schemaObj[key].options,
-                    }))
-                }
-                if (modifiedDoc[key] === null) {
-                    promises.push(rmCb({
-                        data: originalDoc[key],
-                        options: schemaObj[key].options,
-                    }))
-                }
-                if (typeof modifiedDoc[key] === 'object') {
-                    promises.push(
-                        ...this.updateFileAttachments(
-                            originalDoc[key], modifiedDoc[key], schemaObj[key], rmCb, mkCb,
-                        ),
-                    )
-                }
-            })
+  private updateFileAttachments(
+    originalDoc: Document,
+    modifiedDoc: UpdateQuery<FileAttachment> | UpdateWithAggregationPipeline | null,
+    schemaObj: any,
+    rmCb: AttachCallback,
+    mkCb: AttachCallback,
+  ): Promise<void>[] {
+    const promises: Promise<void>[] = []
+    if (modifiedDoc && typeof modifiedDoc === 'object') {
+      if (Array.isArray(modifiedDoc)) {
+        Object.keys(modifiedDoc).map((key) =>
+          promises.push(
+            ...this.updateFileAttachments(
+              originalDoc[key], modifiedDoc[key], schemaObj[key], rmCb, mkCb,
+            ),
+          ))
+      }
+      Object.keys(modifiedDoc).forEach((key) => {
+        if (schemaObj == undefined) return
+        if (modifiedDoc[key] instanceof FileAttachment) {
+          promises.push(mkCb({
+            data: modifiedDoc[key],
+            options: schemaObj[key].options,
+          }))
         }
-        return promises
+        if (modifiedDoc[key] === null) {
+          promises.push(rmCb({
+            data: originalDoc[key],
+            options: schemaObj[key].options,
+          }))
+        }
+        if (typeof modifiedDoc[key] === 'object') {
+          promises.push(
+            ...this.updateFileAttachments(
+              originalDoc[key], modifiedDoc[key], schemaObj[key], rmCb, mkCb,
+            ),
+          )
+        }
+      })
     }
+    return promises
+  }
 
   private findAttachments(doc?: Document): {
     data: FileAttachment
@@ -90,16 +90,16 @@ export default class Controller {
     return attachments.find((at) => at.data && at.data._id === id)
   }
 
-    private async saveFile(attachment: AttachData, doc: Document): Promise<void> {
-        if (!attachment.data) return
-        attachment.data.serviceId = doc.id
-        attachment.data._id = Types.ObjectId().toHexString()
-        const srcPath = attachment.data.path
-        const storage = Storage.from(attachment, doc)
-        attachment.data.path = Path.join(storage.storagePath, attachment.data._id)
-        await storage.fsMkdir(attachment.data._id)
-        await storage.fsCopy(srcPath, Path.join(attachment.data._id, attachment.data.name))
-    }
+  private async saveFile(attachment: AttachData, doc: Document): Promise<void> {
+    if (!attachment.data) return
+    attachment.data.serviceId = doc.id
+    attachment.data._id = Types.ObjectId().toHexString()
+    const srcPath = attachment.data.path
+    const storage = Storage.from(attachment, doc)
+    attachment.data.path = Path.join(storage.storagePath, attachment.data._id)
+    await storage.fsMkdir(attachment.data._id)
+    await storage.fsCopy(srcPath, Path.join(attachment.data._id, attachment.data.name))
+  }
 
   private async removeFile(attachment: AttachData, doc: Document): Promise<void> {
     if (!attachment.data) return
@@ -108,7 +108,7 @@ export default class Controller {
     const filePath = Path.join(attachment.data._id, attachment.data.name)
     await storage.fsRemove(filePath)
     await storage.fsRmdir(attachment.data._id)
-    // await storage.rmStorage()
+    await storage.rmStorage()
   }
 
   saveAttachments(doc: Document): Promise<void[]> {
@@ -117,28 +117,37 @@ export default class Controller {
     return Promise.all(promises)
   }
 
-    updateAttachments(
-        doc: Document,
-        modified: UpdateQuery<FileAttachment> | UpdateWithAggregationPipeline | null,
-    ): Promise<void[]> {
-        const mkCb = (attachment: AttachData): Promise<void> =>
-            this.saveFile(attachment, doc)
+  updateAttachments(
+    doc: Document,
+    modified: UpdateQuery<FileAttachment> | UpdateWithAggregationPipeline | null,
+  ): Promise<void[]> {
+    const mkCb = (attachment: AttachData): Promise<void> =>
+      this.saveFile(attachment, doc)
 
-        const rmCb = (attachment: AttachData): Promise<void> =>
-            this.removeFile(attachment, doc)
+    const rmCb = (attachment: AttachData): Promise<void> =>
+      this.removeFile(attachment, doc)
 
-        const schemaObj = this.schema.obj
-        const promises = this.updateFileAttachments(doc, modified, schemaObj, rmCb, mkCb)
-        return Promise.all(promises)
-    }
+    const schemaObj = this.schema.obj
+    const promises = this.updateFileAttachments(doc, modified, schemaObj, rmCb, mkCb)
+    return Promise.all(promises)
+  }
 
   updateAttachmentsOnSave(original: Document, modified: Document): Promise<void[]> {
-    const attachments = this.findAttachments(original)
-    const modifiedFields = modified.modifiedPaths({ includeChildren: true })
-    const promises = attachments.map(async (attachment) => {
-      if (modifiedFields.includes(attachment.path) && original[attachment.path] !== null) {
-        await this.removeFile(attachment, original)
-        await this.saveFile(attachment, modified)
+    const oldAttachments = this.findAttachments(original)
+    const newAttachments = this.findAttachments(modified)
+    const modifiedPaths = modified.modifiedPaths({ includeChildren: true })
+      .filter((path) => oldAttachments.some((oa) => oa.path === path))
+
+    const promises = modifiedPaths.map(async (path) => {
+      const oldAttachment = oldAttachments.find((oa) => oa.path === path)
+      const newAttachment = newAttachments.find((na) => na.path === path)
+      if (oldAttachment && newAttachment) {
+        if (oldAttachment.data) {
+          await this.removeFile(oldAttachment, original)
+        }
+        if (newAttachment.data) {
+          await this.saveFile(newAttachment, modified)
+        }
       }
     })
     return Promise.all(promises)
